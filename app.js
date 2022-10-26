23b68c86bb78de8a31a6132caa3e1eb5f15754bf
Kevin McPeak <kevmc46@gmail.com>
2022-10-26 09:37:26 -0400

    Added files for heroku deployment

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const mongoose = require("mongoose");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
-const mongoDB =
+const dev_db_url =
 	"mongodb+srv://m001-student:m001-mongodb-basics@sandbox.wdeinur.mongodb.net/movie_database?retryWrites=true&w=majority";
+
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
